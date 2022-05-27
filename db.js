c20560e296ddc76127870af342a7d2c87940b946
Vinicios Pires <contato.viniciospires@gmail.com>
2022-05-27 11:34:33 -0300

    first commit

---
+++ db.js
@@ -0,0 +1,13 @@
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Pool } = pg;
+const db = new Pool({
+	host: "localhost",
+	port: 5432,
+	user: "postgres",
+	password: process.env.SENHA,
+	database: "boardcamp",
+});
