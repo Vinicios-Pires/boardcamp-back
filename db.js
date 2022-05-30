46f2b2187fef462c76098c2c020081ae505fec25
Vinicios Pires <contato.viniciospires@gmail.com>
2022-05-30 02:25:18 -0300

    deploy

--- db.js
+++ db.js
@@ -4,8 +4,17 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const { Pool } = pg;
-const db = new Pool({
-	connectionString: process.env.DATABASE_URI,
-});
+
+const databaseConfig = {
+	connectionString: process.env.DATABASE_URL,
+};
+
+if (process.env.MODE === "PROD") {
+	databaseConfig.ssl = {
+		rejectUnauthorized: false,
+	};
+}
+
+const db = new Pool(databaseConfig);
 
 export default db;
